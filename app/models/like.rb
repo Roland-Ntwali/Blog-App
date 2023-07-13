7d7aeac5dc4857407bc07de6cc70a9d133fc7a7b
Roland-Ntwali <rolandntwali@gmail.com>
2023-07-13 13:46:17 +0200

    Add user, post, like and comments

---
+++ app/models/like.rb
@@ -0,0 +1,10 @@
+class Like < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
+
+  after_create :update_post_likes_counter
+
+  def update_post_likes_counter
+    post.update(likes_count: post.likes.count)
+  end
+end
